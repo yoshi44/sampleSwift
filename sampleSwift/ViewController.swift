75b3d83263c676433e88a80e87eb907988bd7190
yoshi <yoshinihon@yoshis-MacBook-Air.local>
2016-12-04 00:04:59 +0900

    [branch_name] swift init

---
+++ sampleSwift/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  sampleSwift
+//
+//  Created by yoshi on 2016/12/03.
+//  Copyright © 2016 yoshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("Hello Swift!")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
